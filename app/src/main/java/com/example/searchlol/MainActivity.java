32dec9f96dcde8991a68f498e56b5153ef5c05b5
LuSuAllen <zijing.lusuallen@gmail.com>
2020-02-26 18:41:54 -0800

    initial look of main screen with menu

--- app/src/main/java/com/example/searchlol/MainActivity.java
+++ app/src/main/java/com/example/searchlol/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.searchlol;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
