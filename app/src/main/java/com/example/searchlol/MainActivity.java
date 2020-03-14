d1bf5a6b3d3262526c3ec35e3b3215a29ccd33aa
LuSuAllen <zijing.lusuallen@gmail.com>
2020-03-14 02:36:24 -0700

    add drawer and icon for setting on top right

--- app/src/main/java/com/example/searchlol/MainActivity.java
+++ app/src/main/java/com/example/searchlol/MainActivity.java
@@ -1,21 +1,89 @@
 package com.example.searchlol;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    private DrawerLayout mDrawerLayout;
+
+    private EditText mSearchSummonerET;
+
+//    private ProgressBar mLoadingIndicatorPB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+//        getSupportActionBar().setElevation(0);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        Button searchButton = findViewById(R.id.search_button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String summonerName = mSearchSummonerET.getText().toString();
+                if (!TextUtils.isEmpty(summonerName)) {
+
+                }
+
+            }
+        });
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.drawer_menu);
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        mDrawerLayout.closeDrawers();
+        switch (menuItem.getItemId()) {
+            case R.id.nav_search:
+                return true;
+//            case R.id.nav_saved_repos:
+//                Intent savedReposIntent = new Intent(this, SavedReposActivity.class);
+//                startActivity(savedReposIntent);
+//                return true;
+//            case R.id.nav_settings:
+//                Intent settingsIntent = new Intent(this, SettingsActivity.class);
+//                startActivity(settingsIntent);
+//                return true;
+            default:
+                return false;
+        }
     }
 }
