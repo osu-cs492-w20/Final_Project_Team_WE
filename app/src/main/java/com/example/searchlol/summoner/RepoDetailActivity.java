3267c771311115ca9224a971f4404c30a8e7b94e
jinning <zhangji5@oregonstate.edu>
2020-03-15 21:50:17 -0700

    fixed UI

--- app/src/main/java/com/example/searchlol/summoner/RepoDetailActivity.java
+++ app/src/main/java/com/example/searchlol/summoner/RepoDetailActivity.java
@@ -2,8 +2,12 @@ package com.example.searchlol.summoner;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.searchlol.R;
@@ -33,15 +37,37 @@ public class RepoDetailActivity extends AppCompatActivity {
         if (intent != null && intent.hasExtra(EXTRA_GITHUB_REPO)) {
             TextView repoNameTV = findViewById(R.id.tv_summoner_name);
             repoNameTV.setText("Player: "+mUsername);
+            TextView repoRankTV = findViewById(R.id.tv_Rank);
+            repoRankTV.setText("Default: IronV");
             TextView repoLevelTV = findViewById(R.id.tv_summoner_Level);
             repoLevelTV.setText("Level "+mLevel);
-            TextView repoIconTV = findViewById(R.id.tv_summoner_id);//uicon ...textview iconveiw
-            repoIconTV.setText("ICON "+mIcon);
+            ImageView repoIconTV = findViewById(R.id.tv_summoner_id);//uicon ...textview iconveiw
+            //repoIconTV.setImageIcon();
             TextView repoDescriptionTV = findViewById(R.id.tv_summoner_description);
             repoDescriptionTV.setText("About: This player is BronzeV");
 
             //TextView repoStarsTV = findViewById(R.id.tv_repo_stars);
             //repoStarsTV.setText(Integer.toString(mRepo.stargazers_count));
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.repo_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_view_repo:
+                //invoke activity
+                return true;
+            case R.id.action_share:
+                //invoke activity
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
