7fd0404cdbf0793040762afd2e7bcaa7431ba18e
LuSuAllen <zijing.lusuallen@gmail.com>
2020-03-19 21:49:41 -0700

    测试....

--- app/src/main/java/com/example/searchlol/summoner/SummonerDetailActivity.java
+++ app/src/main/java/com/example/searchlol/summoner/SummonerDetailActivity.java
@@ -94,10 +94,6 @@ public class SummonerDetailActivity extends AppCompatActivity implements View.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summoner_detail);
-        if (android.os.Build.VERSION.SDK_INT > 9) {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-        }
 
 
         Intent intent = getIntent();
