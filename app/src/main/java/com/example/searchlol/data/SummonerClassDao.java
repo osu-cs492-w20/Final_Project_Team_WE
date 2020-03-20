4c4e6cd74989a1b149b7b37808fb3e0286ed2497
LuSuAllen <zijing.lusuallen@gmail.com>
2020-03-20 09:03:03 -0700

    加油

--- app/src/main/java/com/example/searchlol/data/SummonerClassDao.java
+++ app/src/main/java/com/example/searchlol/data/SummonerClassDao.java
@@ -3,6 +3,7 @@ package com.example.searchlol.data;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
 import java.util.List;
@@ -10,16 +11,16 @@ import androidx.lifecycle.LiveData;
 
 @Dao
 public interface SummonerClassDao {
-    @Insert
-    void insert(SummonerClass summoner);
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(SummonerClass name);
 
     @Delete
-    void delete(SummonerClass summoner);
+    void delete(SummonerClass name);
 
-    @Query("SELECT * FROM summoner")
+    @Query("SELECT * FROM summoners")
     LiveData<List<SummonerClass>> getAllSummoners();
 
-    @Query("SELECT * FROM summoner WHERE name = :fullName LIMIT 1")
+    @Query("SELECT * FROM summoners WHERE name = :fullName LIMIT 1")
     LiveData<SummonerClass> getSummonerByName(String fullName);
 
 }
