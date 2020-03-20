e878a2128f19ee4ddb174a8bdc51bb902eecb9ef
jinning <zhangji5@oregonstate.edu>
2020-03-19 23:51:15 -0700

    Rank Status Finished

---
+++ app/src/main/java/com/example/searchlol/data/RankClass.java
@@ -0,0 +1,9 @@
+package com.example.searchlol.data;
+
+import java.io.Serializable;
+
+public class RankClass implements Serializable {
+    public String rank;
+    public String tier;
+    public int leaguePoints;
+}
